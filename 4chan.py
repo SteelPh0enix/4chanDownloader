40d5964637eab59927454fa6238c96e0ee753755
PhoenixPL512 <phoenixpl512@gmail.com>
2017-02-11 22:37:15 +0100

    the fucking final script is here

--- 4chan.py
+++ 4chan.py
@@ -1,8 +1,99 @@
-import basc_py4chan as chan
+import basc_py4chan as chanapi
+import requests
+import argparse
+import sys
 import os
-import urllib.request
 
+class Downloader:
+    def __init__(self):
+        self.boards_list = chanapi.get_all_boards()
 
+    def run(self):
+        self.verify_boards()
+        if len(self.board) == 0:
+            print("No existing boards selected!")
+            sys.exit(2)
+        elif self.board[0] == '*':
+            self.boards = chanapi.get_all_boards()
+        else:
+            self.boards = chanapi.get_boards(self.board)
+
+        if self.thread_id != None:
+            self.download_threads(self.boards[0])
+        else:
+            self.download_boards()
+
+    def board_exists(self, board_name):
+        for board in self.boards_list:
+            if board.name == board_name:
+                return True
+        return False
+
+    def thread_exists(self, thread_id):
+        return self.board[0].thread_exists(thread_id)
+
+    def verify_boards(self):
+        if self.board[0] == '*':
+            return
+
+        for f in self.board:
+            if not self.board_exists(f):
+                self.board.remove(f)
+
+    def download_threads(self, board):
+        for tid in self.thread_id:
+            print(" >Thread #{0} at /{1}/:".format(tid, board.name))
+            if (board.thread_exists(tid)):
+                t = board.get_thread(tid)
+                t.expand()
+                thread_files = t.files()
+                thread_files_sum = sum(1 for _ in thread_files)
+                fnum = 1
+                print(" =>Closed/sticky/archived?: {0}/{1}/{2}\n =>Bumplimit/imagelimit hit: {3}/{4}\n =>Posts: {5}\n =>Files: {6}\n =>Topic: {7}".format(
+                t.closed, t.sticky, t.archived, t.bumplimit, t.imagelimit, len(t.all_posts), thread_files_sum, t.topic.text_comment[:50].encode('utf-8')
+                ))
+
+                for thread_file in t.files():
+                    print("{0}/{1}".format(fnum, thread_files_sum))
+                    self.download_image(thread_file, "{0}/{1}/{2}".format(self.directory, board.name, tid))
+                    fnum += 1
+            else:
+                print(" =>Thread is 404 (don't exists or got deleted)")
+            print("")
+
+    def download_boards(self):
+        for b in self.boards:
+            self.thread_id = b.get_all_thread_ids()
+            self.download_threads(b)
+
+    def download_image(self, url, path):
+        file_name = url.split('/')[-1]
+        imgpath = "{0}/{1}".format(path, file_name)
+        if not os.path.exists(path):
+            os.makedirs(path)
+
+        print("Downloading image {0}".format(file_name))
+        response = requests.get(url, stream=True)
+        size = int(response.headers.get('content-length'))
+
+        if os.path.isfile(imgpath) and os.path.getsize(imgpath) == size:
+            print("File is already downloaded!")
+            return
+
+        f = open(imgpath, "wb")
+        if (size is None):
+            f.write(response.content)
+        else:
+            dl = 0
+            for data in response.iter_content(chunk_size=4096):
+                dl += len(data)
+                f.write(data)
+                done = int(50 * dl / size)
+                sys.stdout.write("\r[{0}{1}]".format('=' * done, ' ' * (50-done)))
+                sys.stdout.flush()
+            print("")
+
+'''
 def check_if_board_exists(name):
     boards = chan.get_all_boards()
     for b in boards:
@@ -43,6 +134,16 @@ def main():
         else:
             print("Select an existing board you dumb fuck")
             main()
+'''
 
-if __name__ == "__main__":
-    main()
+parser = argparse.ArgumentParser(description="Download pics from your favourite fucking boards (or threads). Enter board names, or one board name and threads ID's.", epilog="op is a faggot")
+parser.add_argument('-d', '--directory', default="4chan", help="directory or path in which pics will be saved (default: 4chan)")
+parser.add_argument('-b', '--board', help="board(s) short name(s) from where pictures will be downloaded (* means all boards, enter multiple with spaces)", nargs='+')
+parser.add_argument('-t', '--thread_id', help="thread ID's from where pics will be downloaded (you can enter multiple with spaces)", nargs='+')
+
+dl = Downloader()
+args = parser.parse_args(namespace=dl)
+if dl.board == None:
+    print("You must enter at least one board!")
+    sys.exit(1)
+dl.run()
--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # 4chanDownloader
-Simple command-line tool for downloading pics from 4chan boards/threads. IN A FUCKING DEVELOPEMENT, so don't expect much.
+Simple command-line tool for downloading pics from 4chan boards/threads.
 
-Python 3.6, requires this shit to work: https://pypi.python.org/pypi/BASC-py4chan/0.6.3
+#### Based on Python 3.6, requires:
+- this shit to work: https://pypi.python.org/pypi/BASC-py4chan/0.6.3 (pip install basc-py4chan)
+- and requests library: http://docs.python-requests.org/en/master/ (pip install requests)
+
+## Usage:
+'python 4chan.py [-d directory (default: 4chan)] [-b board(s)] [-t thread(s)]''
+
+### Examples:
+Download fucking everything to default directory:
+'python 4chan.py -b *''
+
+Download all /b/ files to default directory
+'python 4chan.py -b b'
+
+Download all /b/, /v/ and /s4s/ files to folder called bullshit
+'python 4chan.py -d bullshit -b b v s4s'
+
+Download threads with ID 2137, 420 and 69 from board /s4s/ to default directory
+'python 4chan.py -b s4s -t 2137 420 69'
+
+You can download selected threads from only one board at time (only 1st board will be used with thread ID's, use script multiple times to download selected threads from selected boards).
+Use -b * to download every file from every board
